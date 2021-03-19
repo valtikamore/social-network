2b488bd5ed76af810f9b69d4573a0a958aaf7455
Valentine <valtikaless@gmail.com>
2021-03-19 17:34:49 +0300

    50-51 Rest API \ axios \ CRUD

--- src/components/users/users.tsx
+++ src/components/users/users.tsx
@@ -1,50 +1,21 @@
 import React, {FC} from 'react';
 import {UsersPropsType} from "./usersContainer";
 import s from './users.module.css'
+import axios from "axios";
+import userWithoutPhoto from '../../assets/imagies/userWithoutPhoto.png'
 
 
 export const Users: FC<UsersPropsType> = ({users, follow, setUsers, unFollow,}) => {
     if (users.length === 0) {
-        setUsers([
-            {
-                id: 1,
-                imgUrl: 'https://i.pinimg.com/originals/ec/a1/18/eca1187492c9ce5ff3b78bf9b9a6ed9a.jpg',
-                follow: true,
-                name: 'Valentine',
-                location: {city: 'Minsk', country: 'Belarus'},
-                status: 'Looking for a job '
-            },
-            {
-                id: 2,
-                imgUrl: 'https://i.pinimg.com/originals/ec/a1/18/eca1187492c9ce5ff3b78bf9b9a6ed9a.jpg',
-                follow: false,
-                name: 'Ivan',
-                location: {city: 'Moscow', country: 'Russia'},
-                status: 'Looking for a job '
-            },
-            {
-                id: 3,
-                imgUrl: 'https://i.pinimg.com/originals/ec/a1/18/eca1187492c9ce5ff3b78bf9b9a6ed9a.jpg',
-                follow: true,
-                name: 'Kummer',
-                location: {city: 'Chemnitz ', country: 'Germany'},
-                status: 'Looking for a job '
-            },
-            {
-                id: 4,
-                imgUrl: 'https://i.pinimg.com/originals/ec/a1/18/eca1187492c9ce5ff3b78bf9b9a6ed9a.jpg',
-                follow: true,
-                name: 'Natalia',
-                location: {city: 'Minsk', country: 'Belarus'},
-                status: 'Looking for a job '
-            },
-        ])
-    }
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(resp => {
+            setUsers(resp.data.items)
+        })
 
+    }
     const usersMap = users.map(user => {
         return (
             <div key={user.id} className={s.grid}>
-                <img className={s.photo} src={user.imgUrl} alt='photo'/>
+                <img className={s.photo} src={user.photos.small !== null ?user.photos.small : userWithoutPhoto} alt='photo'/>
                 <div className={s.follow}>
                     {user.follow
                         ? <button onClick={() => follow(user.id)}>Follow</button>
@@ -52,15 +23,13 @@ export const Users: FC<UsersPropsType> = ({users, follow, setUsers, unFollow,}) 
                 </div>
                 <div className={s.name}>{user.name}</div>
                 <div className={s.status}>{user.status}</div>
-                <div className={s.country}>{user.location.country}</div>
-                <div className={s.city}>{user.location.city}</div>
+                <div className={s.country}>user.location.country</div>
+                <div className={s.city}>user.location.city</div>
             </div>
         )
     })
     return (
-        <div>
             {usersMap}
-        </div>
     );
 };
 
