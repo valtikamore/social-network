1fe58d7a3f55260b86b452b7736c7eb3736380d7
Valentine <valtikaless@gmail.com>
2021-04-02 18:45:31 +0300

    52-2 get users callback

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -2,21 +2,25 @@ import React, {FC} from "react";
 import {usersPropsType} from "./UsersContainer";
 import axios from "axios";
 import userWithoutPhoto from '../../assets/designer.svg'
+
+
 export const Users: FC<usersPropsType> = ({users, follow, unFollow, setUsers}) => {
-    debugger
-    if (users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                setUsers(response.data.items)
-            })
+    let getUsers = () => {
+        if (users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    setUsers(response.data.items)
+                })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>get users </button>
             {users.map(u => {
                 return <div key={u.id}>
                     <img style={{width: '50px', height: '50px'}}
-                         src={u.photos.large || u.photos.small
+                         src={ u.photos.small !== null
                              ? u.photos.small
                              : userWithoutPhoto} alt="user avatar"/>
                     {u.followed
--- src/redux/reducers/user-reducer/users-reducer.ts
+++ src/redux/reducers/user-reducer/users-reducer.ts
@@ -1,24 +1,17 @@
 import {AllActionTypes} from "../../redux-store";
 
-// export type userType = {
-//     id:number
-//     followed:boolean
-//     fullName:string
-//     status:string
-//     location:{city:string,country:string}
-// }
 export type userFromServer = {
     name:string
     id:number
-    photos:{small:string | null,large:string | null}
+    photos:{small:null | string,large:string | null }
     status:string | null
     followed:boolean
 }
-type usersFromServer = {
-    items:userFromServer[]
-    totalCount:number
-    error:string
-}
+// type usersFromServer = {
+//     items:userFromServer[]
+//     totalCount:number
+//     error:string
+// }
 type initialStateType = typeof initialState
 let initialState = {
     users: [
