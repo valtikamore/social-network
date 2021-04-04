a200debe2f430aff07ebd8a139c6812d5c2b2c29
Valentine <valtikaless@gmail.com>
2021-04-04 16:26:04 +0300

    54- life cycle methods/ componentDidMount

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -5,13 +5,13 @@ import userWithoutPhoto from '../../assets/designer.svg'
 
 
 export class Users extends React.Component<usersPropsType>{
-    constructor(props:usersPropsType) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
+
     getUsers = () => {
         if (this.props.users.length === 0) {
 
